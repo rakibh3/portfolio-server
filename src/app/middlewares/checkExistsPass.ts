d07b3edaf1df97f08a2fa2f9bb0cb356e492f904
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-01-03 23:15:31 +0600

    Clean code

--- src/app/middlewares/checkExistsPass.ts
+++ src/app/middlewares/checkExistsPass.ts
@@ -1,15 +1,5 @@
 import bcrypt from 'bcrypt'
 
-// export const checkIfPasswordExists = (
-//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//   previousPasswords: any,
-//   newPassword: string,
-// ) => {
-//   return previousPasswords.some(async (hashedPassword: string) => {
-//     return await bcrypt.compare(newPassword, hashedPassword)
-//   })
-// }
-
 export async function checkPassword(
   plainTextPassword: string,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
